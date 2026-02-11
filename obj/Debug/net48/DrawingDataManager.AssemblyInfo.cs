ee126a18741e286d7d2b6a55c9a4b917529d5eb2
Yarovoy <E1173031@chemelex.com>
2026-02-11 15:53:46 +0100

    Сохранение версии V3

--- obj/Debug/net48/DrawingDataManager.AssemblyInfo.cs
+++ obj/Debug/net48/DrawingDataManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrawingDataManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0dbba4484d8aecd7be705b3d85e8a45217c7eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrawingDataManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrawingDataManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
